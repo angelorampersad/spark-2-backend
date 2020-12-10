cccfaf4ac268aa6bbe9567e41163b0005b5a617d
Angelo Rampersad <angelorampersad94@gmail.com>
2020-12-10 17:35:23 +0100

    app engine deploy

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = {
+    upload: {
+        provider: 'google-cloud-storage',
+        providerOptions: {
+            bucketName: 'spark-2-strapi',
+            publicFiles: false,
+            uniform: false,
+            basePath: '',
+        },
+    },
+}
