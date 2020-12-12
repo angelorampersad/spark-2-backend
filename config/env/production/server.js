50a28fd80fb1c203d3178961b1ba0f17519e19c5
Angelo Rampersad <angelorampersad94@gmail.com>
2020-12-12 16:42:07 +0100

    add development configuration

---
+++ config/env/production/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b6b72cf39f443dbf2fab02e9df608f09'),
+    },
+  },
+});
